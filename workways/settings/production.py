ff8714783421a61a00c9479f80b6276ec5d3b510
Neeraj Shukla <shuklaneeraj2013.2014@gmail.com>
2018-07-09 18:38:42 +0530

    Update production for whitenoise

--- workways/settings/production.py
+++ workways/settings/production.py
@@ -57,6 +57,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,3 +145,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE= 'whitenoise.django.GzipManifestStacticsFilesStorage'
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, "static"),
+)
+# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+#
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#
+#
+# MEDIA_URL = "/media/"
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
