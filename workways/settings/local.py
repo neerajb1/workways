442b374c574a9c21ddff8cda9d67fa8beb652130
Neeraj Shukla <shuklaneeraj2013.2014@gmail.com>
2018-07-10 16:35:46 +0530

    Update Django for whitenoise static

--- workways/settings/local.py
+++ workways/settings/local.py
@@ -150,8 +150,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-# STATICFILES_DIRS = (
-#      os.path.join(BASE_DIR, "static"),
-# )
-# STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
--- workways/wsgi.py
+++ workways/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWithNoise
+# from whitenoise.django import DjangoWithNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workways.settings")
 
 application = get_wsgi_application()
-application = DjangoWithNoise(application)
+# application = DjangoWithNoise(application)
